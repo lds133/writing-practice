f59e5f44db12f06f8b643cc1ea5a3a40f302ec12
lds133 <lds133@gmail.com>
2026-01-22 00:57:58 +0100

    elevenlab sound

--- tts/convert_elevenlabs.py
+++ tts/convert_elevenlabs.py
@@ -2,42 +2,58 @@ from elevenlabs.client import ElevenLabs
 from secret import ELEVENLABS_KEY
 
 
+VOICE_DEFAULT = "m0"
+
 VOICE_DIR = "../voice"
 
-VOICE = {
-  #"pl-PL": ["P9yx385KN0FOmLll8Lkx","Michał K"],#<-- man 1 low 
+VOICE = [
+  ["pl-PL", "P9yx385KN0FOmLll8Lkx","Michał K","m0"],#<-- man 1 low 
+  ["pl-PL", "V5GZ9rfeV9jjKZE5NkT7", "adam", "m2"], # <--
+  ["pl-PL", "eJLcDj3fKW65V8WhDqPI", "null","m3"],# <-   male 
+  ["pl-PL", "lehrjHysCyPSvjt0uSy6", "marta", "f0"], # <-- woman 1
+  ["pl-PL", "zzBTsLBFM6AOJtkr1e9b", "pawel pro", "m1"], # <----- man 2
+  ["pl-PL", "aAY9hMI6VU335JUszdRs", "alexandra","f1"], # <----- femail 2
   
-  
-  #"pl-PL": ["V5GZ9rfeV9jjKZE5NkT7", "adam"], # <--
-  #"pl-PL": ["eJLcDj3fKW65V8WhDqPI", "null"],
-  #"pl-PL": ["lehrjHysCyPSvjt0uSy6", "marta"], # <-- woman 1
- #"pl-PL": ["zzBTsLBFM6AOJtkr1e9b", "pawel pro"], # <----- man 2
- 
- "pl-PL": ["aAY9hMI6VU335JUszdRs", "alexandra"], # <----- femail 2
-  
-  }
+  ]
 
 client = ElevenLabs(api_key=ELEVENLABS_KEY)
 
-def convert(sentence: str, filename: str,language:str)->bool:
+
+
+def find_voice(language:str,voicetype:str):
+
+    for d in VOICE:
+      if (d[0]==language) and (d[3]==voicetype):
+          return d;
+    return None
+
+
+def convert(sentence: str, filename: str,language:str,voicetype:str=None)->bool:
+    
+    if voicetype==None:
+        voicetype = VOICE_DEFAULT;
         
-    if not ( language in VOICE):
-        print(f"Language {language} not supported")
+    d = find_voice(language,voicetype)
+    
+    if d==None:
+        print(f"Voice {language} - {voicetype} not supported")
         return False
         
-    voiceid = VOICE[language][0]
-    voicename = VOICE[language][1]
-    
+        
+        
+    voiceid = d[1]
+    voicename = d[2]
+   
     audio_generator = client.text_to_speech.convert(
         text=sentence,
         voice_id=voiceid,
         output_format="wav_22050",
     )
-
+    
     audio_data = b"".join(audio_generator)
     
     audio_data = audio_data[:-100] # click wolkaround    
-
+    
     with open(filename, "wb") as wav_file:
         wav_file.write(audio_data)
     
