f0839a7d09bbb7ab97ed2e2b5e8315dd2ff70bbc
lds133 <lds133@gmail.com>
2026-01-23 10:47:04 +0100

    more voices

--- tts/convert_elevenlabs.py
+++ tts/convert_elevenlabs.py
@@ -7,12 +7,17 @@ VOICE_DEFAULT = "m0"
 VOICE_DIR = "../voice"
 
 VOICE = [
-  ["pl-PL", "P9yx385KN0FOmLll8Lkx","Michał K","m0"],#<-- man 1 low 
-  ["pl-PL", "V5GZ9rfeV9jjKZE5NkT7", "adam", "m2"], # <--
-  ["pl-PL", "eJLcDj3fKW65V8WhDqPI", "null","m3"],# <-   male 
-  ["pl-PL", "lehrjHysCyPSvjt0uSy6", "marta", "f0"], # <-- woman 1
-  ["pl-PL", "zzBTsLBFM6AOJtkr1e9b", "pawel pro", "m1"], # <----- man 2
-  ["pl-PL", "aAY9hMI6VU335JUszdRs", "alexandra","f1"], # <----- femail 2
+  ["pl-PL", "P9yx385KN0FOmLll8Lkx", "Michał K",   "m","0"],#<-- man 1 low 
+  ["pl-PL", "V5GZ9rfeV9jjKZE5NkT7", "adam",       "m","2"], # <-- male
+  ["pl-PL", "eJLcDj3fKW65V8WhDqPI", "null",       "m","3"],# <-   male 
+  ["pl-PL", "lehrjHysCyPSvjt0uSy6", "marta",      "f","0"], # <-- woman 1
+  ["pl-PL", "zzBTsLBFM6AOJtkr1e9b", "pawel pro",  "m","1"], # <----- man 2
+  ["pl-PL", "aAY9hMI6VU335JUszdRs", "alexandra",  "f","1"], # <----- femail 2
+  
+  ["fr-FR","or4EV8aZq78KWcXw48wd", "rachel_fr","f","0" ],
+  ["es-ES","6wMKsI8ig8FZUfpyZDIY", "nadia_es","f","0" ],
+  ["de-DE","7eVMgwCnXydb3CikjV7a","lea_de","f","0"],
+  ["en-US","uhHbrbsgQVJnH4Sne0ij","anya_en","f","0"],
   
   ]
 
@@ -22,10 +27,21 @@ client = ElevenLabs(api_key=ELEVENLABS_KEY)
 
 def find_voice(language:str,voicetype:str):
 
+    voice_gender = voicetype[0]
+    voice_index = voicetype[1]
+    v0 = None
+    v1 = None
+    v2 = None
     for d in VOICE:
-      if (d[0]==language) and (d[3]==voicetype):
-          return d;
-    return None
+      if (language==d[0]):
+        v0 = d
+        if (voice_gender==d[3]):
+          v1 = d
+          if (voice_index==d[4]):
+             v2 = d
+             break
+          
+    return v2 if v2!=None else (v1 if v1!=None else v0)
 
 
 def convert(sentence: str, filename: str,language:str,voicetype:str=None)->bool:
